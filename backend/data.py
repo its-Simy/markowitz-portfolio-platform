5a61ba0fa81621eea335cd714c6a9fa17869e836
its-Simy <sr5428294@gmail.com>
2026-03-10 20:19:19 -0400

    Created a new method named clean. I plan to make it so it can adjust the dataframe accordingly based on the count. If the count == 1, this would imply sonly one item has been added so we can set the stage. We would first filter for only the dates and then add the closing data accordingly.

--- backend/data.py
+++ backend/data.py
@@ -8,6 +8,19 @@ class DataFetcher:
     def __init__(self,api_key:str,df:pd.DataFrame):
         self.api_key = api_key
         self.df = df
+        self.count = 0
+
+    #This should clean the dataframe.
+    #We only want the closing prices along with a singular time stamp
+    def clean(self,symbol) -> None:
+        if self.count == 1:
+            time = self.df.loc[:,'timestamp']
+            closing_data = self.df.loc[:,'close']
+            self.df = pd.concat([time,closing_data])
+            self.df = self.df.rename(columns={'close':symbol}) 
+            print(self.df)
+
+        #print(self.df.loc[:,'symbol'])#creates a shallow copy of the dataframe using the slide (:).    
     
     #only adjusts Dataframe, so doesn't have to return anything
     def fetch(self, symbol:str) -> None:
@@ -15,8 +28,9 @@ class DataFetcher:
         dataRequest = requests.get(url)# loads csv data.
         #Add data to the datframe
         new_data = pd.read_csv(io.StringIO(dataRequest.text))
-        new_data['symbol'] = symbol
         self.df = pd.concat([self.df,new_data],ignore_index=True)#adjusts current dataframe with the new info
+        self.count += 1
+        print(self.df)
         
 
 if __name__ == "__main__":
@@ -24,4 +38,8 @@ if __name__ == "__main__":
     load_dotenv()
     api_key = os.getenv('ALPHA_VANTAGE_API_KEY')
     df = pd.DataFrame()
-    DataFetcher(api_key,df)
+    data = DataFetcher(api_key,df)
+    data.fetch("IBM")
+    #data.clean("IBM")
+    
+
