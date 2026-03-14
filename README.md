5b6f5ddf9325fad120a6ea9fe05e7967e1c9ae58
its-Simy <sr5428294@gmail.com>
2026-03-14 19:19:33 -0400

    Changes to data.py along with data downloading
    This program was adjusted to actually download data from the api, this was for testing purposes to not hit rate limits. Next we implemented the clean method in order to add everything to the dataframe and adjust the dataframe accordingly.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # markowitz-portfolio-platform
+
+
--- backend/data.py
+++ backend/data.py
@@ -5,41 +5,55 @@ import io
 import pandas as pd
 
 class DataFetcher:
-    def __init__(self,api_key:str,df:pd.DataFrame):
+    def __init__(self,api_key:str):
         self.api_key = api_key
-        self.df = df
+        self.df = pd.DataFrame()
         self.count = 0
 
-    #This should clean the dataframe.
+    #This should clean add all the datasets to the dataframe and cleans up the structure
     #We only want the closing prices along with a singular time stamp
-    def clean(self,symbol) -> None:
-        if self.count == 1:
+    def clean(self,symbol,filepath: Union[str,Path]) -> None:
+        
+        if self.count == 0:
+            self.df = pd.read_csv(filepath)
             time = self.df.loc[:,'timestamp']
             closing_data = self.df.loc[:,'close']
-            self.df = pd.concat([time,closing_data])
-            self.df = self.df.rename(columns={'close':symbol}) 
-            print(self.df)
+            self.df = pd.concat([time,closing_data],axis=1)
+        else:
+            temp_data = pd.read_csv(filepath)
+            closing_data = temp_data.loc[:,'close']
+            self.df = pd.concat([self.df,closing_data],axis=1)
 
-        #print(self.df.loc[:,'symbol'])#creates a shallow copy of the dataframe using the slide (:).    
+        self.df.rename(columns={'close':symbol},inplace=True)
+        self.count += 1 
     
     #only adjusts Dataframe, so doesn't have to return anything
     def fetch(self, symbol:str) -> None:
+
         url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='+ symbol +'&apikey=' + self.api_key + "&datatype=csv"#calls the url, fetches data using requests
         dataRequest = requests.get(url)# loads csv data.
+
         #Add data to the datframe
         new_data = pd.read_csv(io.StringIO(dataRequest.text))
         self.df = pd.concat([self.df,new_data],ignore_index=True)#adjusts current dataframe with the new info
-        self.count += 1
-        print(self.df)
+        #Downloads the csv
+        self.df.to_csv("Google_data",index=False)
+
+    
         
 
 if __name__ == "__main__":
     #loads api key
+    
     load_dotenv()
     api_key = os.getenv('ALPHA_VANTAGE_API_KEY')
-    df = pd.DataFrame()
-    data = DataFetcher(api_key,df)
-    data.fetch("IBM")
-    #data.clean("IBM")
-    
+    data = DataFetcher(api_key)
+    #for now we used data.fetch to downlaod the csv daily data, this allows us to not worry about rate limits
+    #data.fetch("GOOG")
+    data.clean("EFA","Tester_Data\\Barclays_data")
+    data.clean("GOOG","Tester_Data\\Google_data")
+    data.clean("IBM","Tester_Data\\Ibm_data")
+    print(data.df)
+
+
 
