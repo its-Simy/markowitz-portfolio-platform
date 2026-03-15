a342a8c5734b676dedd7d7a1a2bd4eebcf83a7b4
its-Simy <sr5428294@gmail.com>
2026-03-15 14:25:37 -0400

    Proper weights input-mCreated  a method called getWeights. This method asks the user for the weights of each asset. This handles the following edge cases: None float values, input 0 for any asset and sum of all assets not equaling 1

--- backend/calculations.py
+++ backend/calculations.py
@@ -2,10 +2,47 @@ import pandas as pd
 class calculations:
     def __init__(self,df:pd.DataFrame):
         self.df = df
+        self.weights = []
+
+    def getWeights(self) -> None:
+        passed = False
+        isZero = False
+        while not passed:
+            for name in self.df.columns:
+                if name == "timestamp":
+                    continue
+                x = -1
+                while True:
+                    try:
+                        x = float(input(f"What is the current weight for {name}: "))
+                        break
+                    except ValueError:
+                        print("Only integers are valid inputs")
+                
+                if x == 0:
+                    isZero= True
+                self.weights.append(x)
+                        
+            if sum(self.weights) > 1 or sum(self.weights) < 1 or isZero:
+                self.weights = []
+                if(isZero):
+                    print("Invalid input(s), all vlaues must be greater than zero. Try Again")
+                else:
+                    print("Invalid, must input decimals (floats) that sum up to 1, with none being 0. Try Again")
+                isZero = False
+            else:
+                passed = True
+    
+
+            
+        
+    #def expected_return_P(self):
 
 
 if __name__ == "__main__":
-    start = calculations(pd.read_pickle("Tester_Data\\dataset.pkl"))
-    print(start.df)
+    info = calculations(pd.read_pickle("Tester_Data\\dataset.pkl"))
+    info.getWeights()
+    print(info.weights)
+
 
 
