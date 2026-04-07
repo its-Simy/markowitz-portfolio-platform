a2540c40f9e4d26c6a43d3bc789661d508fe2902
its-Simy <sr5428294@gmail.com>
2026-04-06 21:34:09 -0400

    Expected Portfolio return PRE Testing

--- backend/calculations.py
+++ backend/calculations.py
@@ -1,8 +1,11 @@
 import pandas as pd
 class calculations:
     def __init__(self,df:pd.DataFrame):
-        self.df = df
+        self.df = df #time stamps are organized as smaller index = more recent
         self.weights = []
+        self.assetReturns = []
+        self.portfolioReturn = 0
+        
 
     def getWeights(self) -> None:
         passed = False
@@ -32,17 +35,36 @@ class calculations:
                 isZero = False
             else:
                 passed = True
-    
+    #Calculated expected return of an asset within the portfolio
+    def expected_return_asset(self):
+        #we must parse through the dataframe, at point end - 1, until the most recent to use and calculate daily return
+        #backtrack from column length - 2 until 0
+        #(current price - (price location + 1)) / (price location + 1)
+        for label in self.df:
+            if label == 'timestamp':
+                continue
+            count = 0
+            total = 0
+            for price in reversed(self.df.index):
+                if count == 0:
+                    prev = price
+                    count += 1
+                    continue
+                day_price = (price - prev) / prev
+                prev = price
+                total += (day_price * 1/99)
+            self.assetReturns.append(total)
 
-            
-        
-    #def expected_return_P(self):
-
+    #calculate Expected Return of portfolio
+    def expected_return_Portfolio(self):
+        for point in range(len(self.weights)):
+            self.portfolioReturn += (self.weights[point] * self.assetReturns[point])
 
 if __name__ == "__main__":
     info = calculations(pd.read_pickle("Tester_Data\\dataset.pkl"))
-    info.getWeights()
-    print(info.weights)
+    #info.getWeights()
+    #print(info.weights)
+    info.expected_return_asset()
 
 
 
